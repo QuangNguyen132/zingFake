35bebc18ffd10cae30753430db9738a0e127ae44
Your Name <you@example.com>
2021-10-12 22:55:03 +0700

    Add new project zingFake

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+function modalBlock() {
+    var modal = document.querySelector('.modal-theme');
+    modal.classList.add('open');   
+          
+}
+
+function closeModal() {   
+    var modal = document.querySelector('.modal-theme');
+    modal.classList.remove('open');
+}
